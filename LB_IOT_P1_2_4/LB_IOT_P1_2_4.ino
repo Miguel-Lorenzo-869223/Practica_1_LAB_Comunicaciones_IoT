87f6175dcaa70cb3d5792fce245e8706df3f35e3
Miguel-Lorenzo-869223 <869223@unizar.es>
2026-09-17 19:17:57 +0200

    Desarrollo Práctica 1.2.4. Lectura del puerto serie y cambios de estado en función del comando.

---
+++ LB_IOT_P1_2_4/LB_IOT_P1_2_4.ino
@@ -0,0 +1,18 @@
+// ------------------------------------------------------------
+//  Author: Miguel A.Lorenzo
+//  Date: 13/09/2026
+//  Subject: IoT Communications Laboratory 
+//  Master: MSc in Electronic Engineering
+//  University: University of Zaragoza EINA/UNIZAR
+// ------------------------------------------------------------
+#include <Arduino.h>
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
