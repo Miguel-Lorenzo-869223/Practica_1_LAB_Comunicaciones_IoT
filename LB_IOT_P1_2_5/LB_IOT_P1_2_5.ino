a3159e19f736788d3ed4055f2b67537647ec8e61
Miguel-Lorenzo-869223 <869223@unizar.es>
2026-09-17 23:56:23 +0200

    Práctica 1.2.5
    Comunicación I2C entre Arduino NANO 33 BLE como maestro y Arduino NANO 33 IoT como esclavo

--- LB_IOT_P1_2_5/LB_IOT_P1_2_5.ino
+++ LB_IOT_P1_2_5/LB_IOT_P1_2_5.ino
@@ -1,9 +1,35 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Arduino.h>
+#include <Wire.h>
 
+#define I2C_SLAVE_ADDR 8
+#define LED_ON 1
+#define LED_OFF 0
+
+void send_i2c_command(uint8_t cmd);
+
+void setup() {
+  Wire.begin(); // Inicializa I2C como Maestro
+  Serial.begin(115200);
+  while (!Serial && millis() < 4000);
+
+  Serial.println("--- MAESTRO I2C LISTO ---");
+  Serial.println("Enviando comandos '1' y '0' cada segundo...");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Encender LED en la esclava
+  send_i2c_command(LED_ON);
+  Serial.println("I2C -> LED ON");
+  delay(1000);
 
+  // Apagar LED en la esclava
+  send_i2c_command(LED_OFF);
+  Serial.println("I2C -> LED OFF");
+  delay(1000);
+}
+
+void send_i2c_command(uint8_t cmd) {
+  Wire.beginTransmission(I2C_SLAVE_ADDR);
+  Wire.write(cmd);
+  Wire.endTransmission();
 }
---
+++ LB_IOT_P1_2_5/LB_IOT_P1_2_5_slave/LB_IOT_P1_2_5_slave.ino
@@ -0,0 +1,34 @@
+#include <Arduino.h>
+#include <Wire.h>
+
+#define I2C_SLAVE_ADDR 8
+#define LED_PIN 2
+void receiveEvent(int bytes);
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, LOW);
+  Wire.begin(I2C_SLAVE_ADDR); // Escucha en la dirección 8
+  Wire.onReceive(receiveEvent);
+}
+
+void loop() {
+  // Bucle libre, la recepción va por interrupción
+}
+
+void receiveEvent(int bytes) {
+  while (Wire.available() > 0) {
+    int cmd = Wire.read(); // Lee el byte enviado (1 o 0)
+    
+    if (cmd == 1) {
+      digitalWrite(LED_BUILTIN, HIGH);
+      digitalWrite(LED_PIN, HIGH);
+    } else if (cmd == 0) {
+      digitalWrite(LED_BUILTIN, LOW);
+      digitalWrite(LED_PIN, LOW);
+
+    }
+  }
+}
