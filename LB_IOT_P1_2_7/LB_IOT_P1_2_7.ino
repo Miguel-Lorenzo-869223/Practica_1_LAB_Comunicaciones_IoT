b3f0b95847de53a25407051536b6a99c0eb1e20f
Miguel-Lorenzo-869223 <869223@unizar.es>
2026-09-18 20:10:47 +0200

    Práctica 1.2.7

--- LB_IOT_P1_2_7/LB_IOT_P1_2_7.ino
+++ LB_IOT_P1_2_7/LB_IOT_P1_2_7.ino
@@ -1,9 +1,214 @@
-void setup() {
-  // put your setup code here, to run once:
+// ------------------------------------------------------------
+//  Author: Miguel A.Lorenzo
+//  Date: 18/09/2026
+//  Subject: IoT Communications Laboratory 
+//  Master: MSc in Electronic Engineering
+//  University: University of Zaragoza EINA/UNIZAR
+// ------------------------------------------------------------
 
+
+#include <Arduino.h>
+#include <string.h>
+#include "BBTimer.hpp"
+#include "mbed.h"
+#include <Wire.h>
+#include <Arduino_LSM9DS1.h>
+
+#define I2C_SLAVE_ADDR 0x08
+#define BUFFER_LNG 32
+
+
+// Dynamic data variables
+float ax, ay, az;
+float gx, gy, gz;
+float mx, my, mz;
+
+bool finish_write = false;
+int index_bff = 0; 
+char buffer_msg[BUFFER_LNG];
+int x = 0; 
+
+// Hardware Timer and PWM
+BBTimer timer_3_adc(BB_TIMER3);
+
+volatile bool sample = false;
+
+
+unsigned long pv_millis_sample = 0;
+const long interval_sample = 200; // Sample and send every 200 ms
+
+// FUNCTIONS DECLARATION
+void read_imu();
+void printer();
+
+// Timer ISR: runs every period_seconds
+void timerCallback() {
+    sample = true;
+}
+
+// Global control variables
+int period_seconds = 1; // 'x' parameter for send mesagge.
+
+// FUNCTIONS DECLARATION
+void read_serial();
+void parse_command(char* buffer);
+
+void setup() {
+  Wire.begin(); // Master I2C setup
+  Serial.begin(115200);
+  while(!Serial && millis() < 4000);
+  strcpy(buffer_msg, "ST(1)");
+  finish_write = true;
+
+  if (!IMU.begin()) {
+    Serial.println("Failed to initialize IMU!");
+    while (1);
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  unsigned long current_millis = millis();
 
+  read_serial();
+
+  if(current_millis - pv_millis_sample >= interval_sample){
+    pv_millis_sample = current_millis;
+    read_imu();
+  }
+
+  if (finish_write) {
+      parse_command(buffer_msg);
+      index_bff = 0;
+      finish_write = false;
+  }
+
+  if (sample) {
+    printer();
+    Wire.beginTransmission(I2C_SLAVE_ADDR);
+    Wire.write((uint8_t*)&ax, sizeof(ax)); // Envía los 4 bytes del float
+    Wire.endTransmission();
+    sample=false;
+  }
 }
+
+void read_serial(){
+  while(Serial.available() > 0 && !finish_write){
+    char c = Serial.read(); 
+    if(c == '\n' || c == '\r'){
+      if(index_bff > 0){
+        buffer_msg[index_bff] = '\0';
+        finish_write = true; 
+      }
+    } else if(index_bff < BUFFER_LNG - 1){
+      buffer_msg[index_bff] = c;
+      index_bff++;
+    }
+  }
+}
+
+void parse_command(char* buffer) {
+  if (sscanf(buffer, "ST(%d)", &x) == 1) {
+    if (x > 0) {
+      period_seconds = x;
+      timer_3_adc.timerStop();
+      timer_3_adc.setupTimer(period_seconds * 1000000UL, timerCallback);
+      timer_3_adc.timerStart();
+    } else {
+      timer_3_adc.timerStop();
+    }
+  }   
+  // Invalid command
+  else {
+    Serial.println("Unrecognized command. Valid formats: ST(X)");
+  }
+}
+
+void read_imu(){
+  if (IMU.accelerationAvailable()) {
+    IMU.readAcceleration(ax, ay, az);
+  }
+  // 2. Read Gyroscope (dps)
+  if (IMU.gyroscopeAvailable()) {
+    IMU.readGyroscope(gx, gy, gz);
+  }
+  // 3. Read Magnetometer (uT)
+  if (IMU.magneticFieldAvailable()) {
+    IMU.readMagneticField(mx, my, mz);
+  }
+}
+
+void printer(){
+  Serial.print("ACC [g]: "); Serial.print(ax); Serial.print(", "); Serial.print(ay); Serial.print(", "); Serial.println(az);
+  Serial.print("GYR [dps]: "); Serial.print(gx); Serial.print(", "); Serial.print(gy); Serial.print(", "); Serial.println(gz);
+  Serial.print("MAG [uT]: "); Serial.print(mx); Serial.print(", "); Serial.print(my); Serial.print(", "); Serial.println(mz);
+  Serial.println("-------------------------------------");
+}
+
+
+
+
+/*
+#include <Arduino.h>
+#include <Wire.h>
+#include <Arduino_LSM9DS1.h>
+
+#define I2C_SLAVE_ADDR 0x08
+
+// Dynamic data variables
+float ax, ay, az;
+float gx, gy, gz;
+float mx, my, mz;
+
+unsigned long previousMillis = 0;
+const long interval = 5000; // Sample and send every 500 ms
+
+void setup() {
+  Wire.begin(); // Master I2C setup
+  Serial.begin(115200);
+  while (!Serial && millis() < 4000);
+
+  if (!IMU.begin()) {
+    Serial.println("Failed to initialize IMU!");
+    while (1);
+  }
+
+  Serial.println("--- IMU READINGS & I2C MASTER READY ---");
+}
+
+void loop() {
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+
+    // 1. Read Accelerometer (g)
+    if (IMU.accelerationAvailable()) {
+      IMU.readAcceleration(ax, ay, az);
+    }
+
+    // 2. Read Gyroscope (dps)
+    if (IMU.gyroscopeAvailable()) {
+      IMU.readGyroscope(gx, gy, gz);
+    }
+
+    // 3. Read Magnetometer (uT)
+    if (IMU.magneticFieldAvailable()) {
+      IMU.readMagneticField(mx, my, mz);
+    }
+
+    // Print values locally to Serial Monitor
+    Serial.print("ACC [g]: "); Serial.print(ax); Serial.print(", "); Serial.print(ay); Serial.print(", "); Serial.println(az);
+    Serial.print("GYR [dps]: "); Serial.print(gx); Serial.print(", "); Serial.print(gy); Serial.print(", "); Serial.println(gz);
+    Serial.print("MAG [uT]: "); Serial.print(mx); Serial.print(", "); Serial.print(my); Serial.print(", "); Serial.println(mz);
+    Serial.println("-------------------------------------");
+
+    // Send primary Accelerometer value (X-axis) formatted via I2C to Slave
+    char payload[16];
+    snprintf(payload, sizeof(payload), "AX:%.2f", ax);
+
+    Wire.beginTransmission(I2C_SLAVE_ADDR);
+    Wire.write(payload);
+    Wire.endTransmission();
+  }
+}
+*/
---
+++ LB_IOT_P1_2_7/LB_IOT_P1_2_7_slave/LB_IOT_P1_2_7_slave.ino
@@ -0,0 +1,101 @@
+// ------------------------------------------------------------
+//  Author: Miguel A.Lorenzo
+//  Date: 17/09/2026
+//  Subject: IoT Communications Laboratory 
+//  Master: MSc in Electronic Engineering
+//  University: University of Zaragoza EINA/UNIZAR
+// ------------------------------------------------------------
+
+// ------------------------------------------------------------
+//  Author: Miguel A.Lorenzo
+//  Date: 17/09/2026
+//  Subject: IoT Communications Laboratory 
+//  Master: MSc in Electronic Engineering
+//  University: University of Zaragoza EINA/UNIZAR
+// ------------------------------------------------------------
+
+#include <Arduino.h>
+#include <Wire.h>
+
+#define I2C_SLAVE_ADDR 0x08
+
+volatile bool newData = false;
+float imuValue = 0.0f;   // Valor recibido del maestro
+
+void receiveEvent(int bytes);
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial && millis() < 4000);
+
+  Wire.begin(I2C_SLAVE_ADDR);
+  Wire.onReceive(receiveEvent);
+
+  Serial.println("--- I2C SLAVE READY ---");
+}
+
+void loop() {
+  if (newData) {
+    Serial.print("Received float from Master IMU: ");
+    Serial.println(imuValue, 3);   // Mostrar con precisión
+    newData = false;
+  }
+}
+
+void receiveEvent(int bytes) {
+  if (bytes == sizeof(float)) {
+    uint8_t raw[4];
+
+    for (int i = 0; i < 4; i++) {
+      raw[i] = Wire.read();
+    }
+
+    // Copiar bytes al float
+    memcpy(&imuValue, raw, sizeof(float));
+
+    newData = true;
+  }
+}
+
+
+
+/*
+#include <Arduino.h>
+#include <Wire.h>
+
+#define I2C_SLAVE_ADDR 0x08
+#define BUFFER_SIZE 16
+
+char receivedMsg[BUFFER_SIZE];
+volatile bool newData = false;
+
+void receiveEvent(int bytes);
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial && millis() < 4000);
+
+  Wire.begin(I2C_SLAVE_ADDR);
+  Wire.onReceive(receiveEvent);
+
+  Serial.println("--- I2C SLAVE READY ---");
+}
+
+void loop() {
+  if (newData) {
+    Serial.print("Received from Master IMU: ");
+    Serial.println(receivedMsg);
+    newData = false;
+  }
+}
+
+void receiveEvent(int bytes) {
+  int i = 0;
+  while (Wire.available() > 0 && i < BUFFER_SIZE - 1) {
+    receivedMsg[i] = Wire.read();
+    i++;
+  }
+  receivedMsg[i] = '\0'; // Null-terminate string
+  newData = true;
+}
+*/
